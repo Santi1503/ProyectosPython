dd81d645930e44848d99840929a4d32587151285
santiago.gomez1 <santiago.gomez1@alumnos.uneatlantico.es>
2023-10-03 15:50:58 +0200

    Adding QR app

---
+++ Proyectos/qrCode.py
@@ -0,0 +1,12 @@
+import qrcode
+
+data = "https://www.instagram.com/santigt1503/"
+
+qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+img.save("instagramQR.png")
