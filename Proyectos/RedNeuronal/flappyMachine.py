2649ccc77dc9136d60b8a758d1b494d24be5e195
santiago.gomez1 <santiago.gomez1@alumnos.uneatlantico.es>
2023-10-19 15:59:16 +0200

    FIX some attributes and adjustments

--- Proyectos/RedNeuronal/flappyMachine.py
+++ Proyectos/RedNeuronal/flappyMachine.py
@@ -18,6 +18,10 @@ class Bird:
         self.gravity = 1
         self.jump_strength = -10
         self.bird_img = pygame.Rect(self.x, self.y, 50, 50)
+        self.score = 0
+        self.stay_time = 0
+        self.lost_up = False
+        self.lost_down = False
 
     def jump(self):
         self.vel += self.jump_strength
@@ -47,7 +51,8 @@ class Pipe:
         pygame.draw.rect(win, WHITE, self.bottom_pipe)
 
 def main():
-    bird = Bird()
+    num_birds = 1
+    birds = [Bird() for _ in range(num_birds)]
     pipes = [Pipe(WIDTH)]
     clock = pygame.time.Clock()
 
@@ -62,79 +67,92 @@ def main():
     optimizer = tf.keras.optimizers.Adam(learning_rate)
 
     generations = 0
-    best_score = 0
-    score = 0
+    best_scores = [0] * num_birds
+    current_scores = [0] * num_birds
 
-    while True:  # Bucle para las generaciones
+    while True:
         generations += 1
-        print(f"Generation: {generations}, Actual Score: {score} Best Score: {best_score}")
+        print(f"Generation: {generations}, Best Score: {max(best_scores)}")
 
-        bird = Bird()
-        pipes = [Pipe(WIDTH)]
-        run = True
+        for i in range(num_birds):
+            birds[i] = Bird()
+            pipes = [Pipe(WIDTH)]
+            run = True
 
-        while run:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
+            while run:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
 
-            for pipe in pipes:
-                pipe.move(5)
+                for pipe in pipes:
+                    pipe.move(5)
 
-                bird_y = bird.y
-                pipe_x = pipe.x
-                pipe_top_y = pipe.height
-                pipe_bottom_y = pipe.height + 150
+                    bird_y = birds[i].y
+                    pipe_x = pipe.x
+                    pipe_top_y = pipe.height
+                    pipe_bottom_y = pipe.height + 150
 
-                state = [bird_y, pipe_x, pipe_top_y, pipe_bottom_y, bird.vel]
+                    state = [bird_y, pipe_x, pipe_top_y, pipe_bottom_y, birds[i].vel]
 
-                if np.random.rand() < epsilon:
-                    action = np.random.randint(2)
-                else:
-                    action = np.argmax(model.predict(np.array([state])))
+                    if np.random.rand() < epsilon:
+                        action = np.random.randint(2)
+                    else:
+                        action = np.argmax(model.predict(np.array([state])))
 
-                if action == 1:
-                    bird.jump()
+                    if action == 1:
+                        birds[i].jump()
 
-                bird.move()
+                    birds[i].move()
 
-                if bird.y > HEIGHT or bird.y < 0:
-                    run = False
+                    if birds[i].y > HEIGHT or birds[i].y < 0:
+                        run = False
+                        birds[i].lost_up = birds[i].y < 0
+                        birds[i].lost_down = birds[i].y > HEIGHT
 
-                if pipe.x + 50 < 0:
-                    pipes.pop(0)
-                    pipes.append(Pipe(WIDTH))
-                    score += 1
+                    if pipe.x + 50 < 0:
+                        pipes.pop(0)
+                        new_pipe = Pipe(WIDTH)
+                        pipes.append(new_pipe)
+                        if pipe.x < birds[i].x:
+                            birds[i].score += 1
+                            birds[i].stay_time += 1
+                        else:
+                            birds[i].stay_time = 0
 
-                if bird.bird_img.colliderect(pipe.top_pipe) or bird.bird_img.colliderect(pipe.bottom_pipe):
-                    run = False
+                    if birds[i].bird_img.colliderect(pipe.top_pipe) or birds[i].bird_img.colliderect(pipe.bottom_pipe):
+                        run = False
 
-            if not run:
-                target = score
+                if not run:
+                    if birds[i].lost_up or birds[i].lost_down:
+                        reward = -100000
+                    else:
+                        reward = birds[i].score + birds[i].stay_time
 
-                if score > best_score:
-                    best_score = score
+                    if reward > best_scores[i]:
+                        best_scores[i] = reward
 
-                model = tf.keras.Sequential([
-                    tf.keras.layers.Dense(24, input_shape=(5,), activation='relu'),
-                    tf.keras.layers.Dense(2, activation='linear')
-                ])
-                optimizer = tf.keras.optimizers.Adam(learning_rate)
+                    target = reward
 
-                with tf.GradientTape() as tape:
-                    predictions = model(np.array([state]))
-                    loss = tf.reduce_sum(tf.square(target - predictions))
-                grads = tape.gradient(loss, model.trainable_variables)
-                optimizer.apply_gradients(zip(grads, model.trainable_variables))
+                    model = tf.keras.Sequential([
+                        tf.keras.layers.Dense(24, input_shape=(5,), activation='relu'),
+                        tf.keras.layers.Dense(2, activation='linear')
+                    ])
+                    optimizer = tf.keras.optimizers.Adam(learning_rate)
 
-            win.fill((0, 0, 0))
-            bird.draw()
-            for pipe in pipes:
-                pipe.draw()
+                    with tf.GradientTape() as tape:
+                        predictions = model(np.array([state]))
+                        loss = tf.reduce_sum(tf.square(target - predictions))
+                    grads = tape.gradient(loss, model.trainable_variables)
+                    optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
-            pygame.display.update()
-            clock.tick(30)
+                win.fill((0, 0, 0))
+                birds[i].draw()
+                for pipe in pipes:
+                    pipe.draw()
+
+                pygame.display.update()
+                clock.tick(30)
 
 if __name__ == '__main__':
     main()
